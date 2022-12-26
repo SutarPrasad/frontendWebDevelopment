a0e8ee0fb1695a2858602ce49075398b8c56949c
Sutar Prasad <sutar.prasad@yahoo.com>
2022-12-26 21:58:57 +0000

    Exercise 1: Created login.html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PoolCarz';
+  loginTitle = "LogIN"
+  constructor(){}
+  ngOnInit(): void {
+    console.log("App Component created and initialized")
+  }
+
+  login(){
+    alert("Login Clicked")
+  }
 }
